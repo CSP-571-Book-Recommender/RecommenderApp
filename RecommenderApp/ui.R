0790696119649a508073b1ea51891a809a449e37
eshna sengupta <eshna1995@hotmail.com>
2018-11-21 19:51:32 -0600

    initial commit

---
+++ RecommenderApp/ui.R
@@ -0,0 +1,49 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+#install.packages("shinydashboard")
+library(shiny)
+library(shinydashboard)
+
+# Define UI for application that draws a histogram
+shinyUI(
+  
+  dashboardPage(
+    skin = "purple",
+  dashboardHeader(title = "Let's get some book recommendations!",
+                  titleWidth = 500),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    
+    fluidRow(tags$head(tags$style(HTML("
+                                div.actionButton {
+                                      text-align: center;
+                                      }
+                                      div.box-header {
+                                      text-align: center;
+                                      }
+                                      "))),
+      box(width = 12, title = "Rate Some Books", background = "purple", solidHeader = TRUE, collapsible = TRUE,
+          div(class = "padding-left: 5px; 
+              height: 450px; 
+              overflow-y: scroll;",
+              uiOutput('ratings')
+          )
+      )
+    ),
+    
+    fluidRow(
+      box(width = 12, title = "Books You May Like", background = "purple", solidHeader = TRUE, collapsible = TRUE,
+          #actionButton("myButton", "Get Recommendations", style='padding:4px'),
+          div(style="display:inline-block;width:100%;text-align: center;",actionButton("myButton", "Get Recommendations", icon = icon("book")))
+          
+      )
+      
+    )
+  )
+))
